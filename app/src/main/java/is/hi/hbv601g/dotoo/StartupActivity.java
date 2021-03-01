e6f5841adaf3d9f910438bc8dde26fd3d6d70ec9
Kari <kari_steinn@outlook.com>
2021-03-01 18:30:49 +0000

    Startup og login útlit

--- app/src/main/java/is/hi/hbv601g/dotoo/StartupActivity.java
+++ app/src/main/java/is/hi/hbv601g/dotoo/StartupActivity.java
@@ -2,13 +2,30 @@ package is.hi.hbv601g.dotoo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartupActivity extends AppCompatActivity {
+
+    private Button mLoginButton;
+    private Button mSignupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_startup);
+
+        mLoginButton = (Button) findViewById(R.id.button_startup_login);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(StartupActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+        mSignupButton = (Button) findViewById(R.id.button_startup_signup);
+
     }
 }
