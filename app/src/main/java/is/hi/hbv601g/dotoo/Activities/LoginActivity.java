ad81ed0ae1bdd598c7e9d327e233cda6bd88074d
Kari <kari_steinn@outlook.com>
2021-03-04 00:40:24 +0000

    Login virkni

--- app/src/main/java/is/hi/hbv601g/dotoo/Activities/LoginActivity.java
+++ app/src/main/java/is/hi/hbv601g/dotoo/Activities/LoginActivity.java
@@ -59,21 +59,16 @@ public class LoginActivity extends AppCompatActivity {
                 JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, json, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
+                        System.out.println(response);
+                        Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                        String user = "Vitlaust nafn";
                         try {
-                            JSONArray jsonArray = response.getJSONArray("user");
-                            for(int i = 0; i < jsonArray.length(); i++){
-                                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                String username = jsonObject.getString("username");
-                                String password = jsonObject.getString("password");
-
-                            }
-                            Intent i = new Intent(LoginActivity.this, HomeActivity.class);
-                            i.putExtra("is.hi.hbv601g.dotoo.user_result", "fakeUser");
-                            startActivity(i);
+                            user = response.getString("name");
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-                        System.out.println(response);
+                        i.putExtra("is.hi.hbv601g.dotoo.user_result", user);
+                        startActivity(i);
                     }
                 }, new Response.ErrorListener() {
                     @Override
