859103dbf46106f53bf22c3728b780063b3ff759
Kari <kari_steinn@outlook.com>
2021-03-07 23:14:36 +0000

    TodoList grunnvirkni

--- app/src/main/java/is/hi/hbv601g/dotoo/client/ExpandableListAdapter.java
+++ app/src/main/java/is/hi/hbv601g/dotoo/client/ExpandableListAdapter.java
@@ -1,4 +1,106 @@
 package is.hi.hbv601g.dotoo.client;
 
-public class ExpandableListAdapter {
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.List;
+
+import is.hi.hbv601g.dotoo.Model.TodoList;
+import is.hi.hbv601g.dotoo.Model.TodoListItem;
+import is.hi.hbv601g.dotoo.R;
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+
+    private Context mContext;
+    private List<TodoList> mTodoLists;
+    private HashMap<TodoList, List<TodoListItem>> mTodoListItems;
+
+    public ExpandableListAdapter(Context context, List<TodoList> todoLists,
+                                 HashMap<TodoList, List<TodoListItem>> todoListItems) {
+        this.mContext = context;
+        this.mTodoLists = todoLists;
+        this.mTodoListItems = todoListItems;
+    }
+
+
+    @Override
+    public int getGroupCount() {
+        return this.mTodoLists.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this.mTodoListItems.get(this.mTodoLists.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this.mTodoLists.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return this.mTodoListItems.get(this.mTodoLists.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        TodoList todoList = (TodoList) getGroup(groupPosition);
+        if(convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this.mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.todolist_group, null);
+        }
+
+        TextView listText = (TextView) convertView.findViewById(R.id.todolist_title);
+        listText.setText(todoList.getName());
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        TodoListItem todoListItem = (TodoListItem) getChild(groupPosition, childPosition);
+
+        if(convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this.mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.todolist_item, null);
+        }
+
+        CheckBox itemCheckBox = (CheckBox) convertView.findViewById(R.id.todolist_item_checkbox);
+        //itemCheckBox.setChecked(todoListItem.getChecked());
+
+        TextView itemText = (TextView) convertView.findViewById(R.id.todolist_item_text);
+        itemText.setText(todoListItem.getDescription());
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
 }
