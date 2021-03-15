595e29d9da657927067486be849a6fd85b6b0c06
Kari <kari_steinn@outlook.com>
2021-03-15 23:55:16 +0000

    Comment á ExpandableListAdapter

--- app/src/main/java/is/hi/hbv601g/dotoo/client/ExpandableListAdapter.java
+++ app/src/main/java/is/hi/hbv601g/dotoo/client/ExpandableListAdapter.java
@@ -25,6 +25,10 @@ import is.hi.hbv601g.dotoo.Model.TodoList;
 import is.hi.hbv601g.dotoo.Model.TodoListItem;
 import is.hi.hbv601g.dotoo.R;
 
+/**
+ * Class that takes care of all functionality when interacting with todolists on the UI.
+ *
+ */
 public class ExpandableListAdapter extends BaseExpandableListAdapter {
 
     private Context mContext;
@@ -34,6 +38,14 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     private TodoListItem mNewItem;
     private ExpandableListView mListView;
 
+    /**
+     * Constructor
+     * @param context Current state of the application
+     * @param todoLists Users todolists
+     * @param deletedLists List of ids of todolists that have been deleted
+     * @param changedTodoLists List of todolists that have been changed
+     * @param listView ExpandableListView that the todolists are in
+     */
     public ExpandableListAdapter(Context context, List<TodoList> todoLists, List<Long> deletedLists,
                                  List<TodoList> changedTodoLists, ExpandableListView listView) {
         this.mContext = context;
@@ -43,49 +55,100 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         this.mChangedTodoLists = changedTodoLists;
     }
 
+    /**
+     * Gets the number of todolists and returns it
+     * @return number of todolists
+     */
     @Override
     public int getGroupCount() {
         return this.mTodoLists.size();
     }
 
+    /**
+     * Gets the number of items for one todolist and returns it
+     * @param groupPosition todolist position in the list of todolists
+     * @return number of items for a todolist
+     */
     @Override
     public int getChildrenCount(int groupPosition) {
         return this.mTodoLists.get(groupPosition).getItems().size();
     }
 
+    /**
+     * Gets a todolist
+     * @param groupPosition todolist position in the list of todolists
+     * @return TodoList
+     */
     @Override
     public Object getGroup(int groupPosition) {
         return this.mTodoLists.get(groupPosition);
     }
 
+    /**
+     * Gets an item in a todolist
+     * @param groupPosition todolist position in the list of todolists
+     * @param childPosition TodoListItem position in the list of todolist items
+     * @return TodoListItem
+     */
     @Override
     public Object getChild(int groupPosition, int childPosition) {
         return this.mTodoLists.get(groupPosition).getItems().get(childPosition);
     }
 
+    /**
+     * Gets the position of a todolist
+     * @param groupPosition todolist position in the list of todolists
+     * @return int, todolist position in the list of todolists
+     */
     @Override
     public long getGroupId(int groupPosition) {
         return groupPosition;
     }
 
+    /**
+     * Gets the position of a TodoListItem
+     * @param groupPosition todolist position in the list of todolists
+     * @param childPosition TodoListItem position in the list of TodoListItems
+     * @return int, TodoListItem position in the list of TodoListItems
+     */
     @Override
     public long getChildId(int groupPosition, int childPosition) {
         return childPosition;
     }
 
+    /**
+     * Indicates whether group and child ids are stable across changes to the underlying data
+     * @return false
+     */
     @Override
     public boolean hasStableIds() {
         return false;
     }
 
+    /**
+     * Gets the ids of the TodoLists that have been deleted
+     * @return List of ids of deleted todolists
+     */
     public List<Long> getDeletedLists() {
         return this.mDeletedListIds;
     }
 
+    /**
+     * Sets the list of deleted TodoLists
+     * @param mDeletedLists list of deleted TodoLists
+     */
     public void setDeletedLists(List<Long> mDeletedLists) {
         this.mDeletedListIds = mDeletedLists;
     }
 
+    /**
+     * Gets a View that displays the given group.
+     * @param groupPosition todolist position in the list of todolists
+     * @param isExpanded if the given todolist is expanded or not
+     * @param convertView the old View to use if it exists
+     * @param parent the parent that this view will eventually be attached to
+     * @return the View corresponding to the group at the specified position
+     */
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
         TodoList todoList = (TodoList) getGroup(groupPosition);
@@ -102,11 +165,11 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 
         Button newItemButton = (Button) convertView.findViewById(R.id.todolist_newitem);
         newItemButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 mNewItem = new TodoListItem();
                 mTodoLists.get(groupPosition).addItem(mNewItem);
+                // Show keyboard when adding an item so the user can edit it
                 InputMethodManager imm =
                         (InputMethodManager)mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
@@ -144,7 +207,6 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
                 currentDeleted.add(mTodoLists.get(groupPosition).getId());
                 setDeletedLists(currentDeleted);
                 mTodoLists.remove(groupPosition);
-                System.out.println("currently deleted" + getDeletedLists());
                 notifyDataSetChanged();
             }
         });
@@ -152,6 +214,15 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         return convertView;
     }
 
+    /**
+     *
+     * @param groupPosition todolist position in the list of todolists
+     * @param childPosition TodoListItem position in the list of TodoListItems
+     * @param isLastChild boolean: whether the TodoListItem is the last child within the TodoList
+     * @param convertView the old View to use if it exists
+     * @param parent the parent that this view will eventually be attached to
+     * @return the View corresponding to the child at the specified position
+     */
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
         TodoListItem todoListItem = (TodoListItem) getChild(groupPosition, childPosition);
@@ -169,7 +240,6 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         itemCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                System.out.println("Erum í onCheckedChanged");
                 if(buttonView.isPressed()) {
                     todoListItem.setChecked(!todoListItem.getChecked());
                     todoListChanged(mTodoLists.get(groupPosition));
@@ -184,10 +254,10 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         itemText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                System.out.println("Erum í onFocusChange");
                 if(!hasFocus) {
-                    System.out.println("Erum í if-setningu í onFocusChange");
                     String text = itemText.getText().toString();
+                    // If EditText has lost focus, set the text to the corresponding
+                    // TodoListItem or delete it if the EditText field is empty
                     if(text.length() == 0 && todoListItem != mNewItem) {
                         mTodoLists.get(groupPosition).getItems().remove(childPosition);
                         notifyDataSetChanged();
@@ -197,9 +267,6 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
                     }
                     todoListChanged(mTodoLists.get(groupPosition));
                     itemText.clearFocus();
-//                    InputMethodManager imm =
-//                            (InputMethodManager)mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
-//                    imm.hideSoftInputFromWindow(itemText.getWindowToken(), 0);
                 } else {
                     itemText.requestFocus();
                     itemText.setSelection(itemText.getText().length());
@@ -209,7 +276,6 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 
         Button deleteItemButton = (Button) convertView.findViewById(R.id.todolist_item_delete);
         deleteItemButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 mTodoLists.get(groupPosition).getItems().remove(childPosition);
@@ -225,11 +291,22 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         return convertView;
     }
 
+    /**
+     * Whether the child in the specified position is selectable
+     * @param groupPosition todolist position in the list of todolists
+     * @param childPosition TodoListItem position in the list of TodoListItems
+     * @return boolean, whether the child in the specified position is selectable
+     */
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
         return true;
     }
 
+    /**
+     * Sets the header color for a TodoList on the View
+     * @param view View for  the TodoList
+     * @param list TodoList
+     */
     public void setHeaderColor(View view, TodoList list) {
         switch (list.getColor()) {
             case "yellow":
@@ -256,6 +333,11 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         }
     }
 
+    /**
+     * Sets the item color for a TodoList on the View
+     * @param view View for the TodoList
+     * @param list TodoList
+     */
     public void setItemColor(View view, TodoList list) {
         switch (list.getColor()) {
             case "yellow":
@@ -282,14 +364,17 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         }
     }
 
+    /**
+     * Checks if a TodoList has already been changed, if not,
+     * add it to the list of changed TodoLists
+     * @param changedTodoList TodoList that has been changed
+     */
     public void todoListChanged(TodoList changedTodoList) {
-        System.out.println("Erum í todoListChanged");
         for(TodoList list: mChangedTodoLists) {
             if (list == changedTodoList) {
                 return;
             }
         }
         mChangedTodoLists.add(changedTodoList);
-        System.out.println("Todolista breytt, fjöldi breyttra: " + mChangedTodoLists.size());
     }
 }
