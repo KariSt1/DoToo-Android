6ced8b43751d6c7c81a3fa4e6037831c552f4461
annamaria99 <ams58@hi.is>
2021-03-15 00:02:47 +0000

    Virkar að búa til event en alltaf sami dagur

--- app/src/main/java/is/hi/hbv601g/dotoo/Fragments/NewEventDialogFragment.java
+++ app/src/main/java/is/hi/hbv601g/dotoo/Fragments/NewEventDialogFragment.java
@@ -2,7 +2,9 @@ package is.hi.hbv601g.dotoo.Fragments;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -14,14 +16,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import is.hi.hbv601g.dotoo.R;
 
 public class NewEventDialogFragment extends DialogFragment {
 
     public interface NoticeDialogListener {
-        public void onDialogPositiveClick(String title);
+        public void onDialogPositiveClick(String title, Calendar startTime, Calendar endTime);
     }
 
     NoticeDialogListener mListener;
@@ -47,6 +56,97 @@ public class NewEventDialogFragment extends DialogFragment {
         View view = inflater.inflate(R.layout.fragment_new_event_dialog, null);
 
         final EditText mTitle = (EditText)view.findViewById(R.id.new_event_title);
+        final EditText mStartDate = (EditText) view.findViewById(R.id.new_event_startDate);
+        final Button mStartTime = (Button) view.findViewById(R.id.new_event_startTime);
+        final Button mEndDate = (Button) view.findViewById(R.id.new_event_endDate);
+        final Button mEndTime = (Button)view.findViewById(R.id.new_event_endTime);
+        Calendar sd = Calendar.getInstance();
+        Calendar ed = Calendar.getInstance();
+
+        // Show a datepicker when the dateButton is clicked
+        mStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cal = Calendar.getInstance();
+
+                DatePickerDialog dpd = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        sd.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        sd.set(Calendar.MONTH, month+1);
+                        sd.set(Calendar.YEAR, year);
+                        mStartDate.setText(dayOfMonth + "-" + (month + 1) + "-" + year);
+
+                    }
+                    }, cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DATE));
+                dpd.show();
+            }
+        });
+
+        // Show a timepicker when the dateButton is clicked
+        mStartTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Calendar now = Calendar.getInstance();
+                final Calendar cal = Calendar.getInstance();
+
+                TimePickerDialog tpd = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hour, int minute) {
+                        sd.set(Calendar.HOUR_OF_DAY, hour);
+                        sd.set(Calendar.MINUTE, minute);
+                        mStartTime.setText(hour + ":" + minute);
+
+                    }
+                }, cal.get(Calendar.HOUR),cal.get(Calendar.MINUTE),true);
+                tpd.show();
+            }
+        });
+
+        // Show a datepicker when the dateButton is clicked
+        mEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Calendar now = Calendar.getInstance();
+                final Calendar cal = Calendar.getInstance();
+
+                DatePickerDialog dpd = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        ed.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        ed.set(Calendar.MONTH, month+1);
+                        ed.set(Calendar.YEAR, year);
+                        mEndDate.setText(dayOfMonth + "-" + (month + 1) + "-" + year);
+
+                    }
+                }, cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DATE));
+                dpd.show();
+            }
+        });
+
+        // Show a timepicker when the dateButton is clicked
+        mEndTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Calendar now = Calendar.getInstance();
+                final Calendar cal = Calendar.getInstance();
+
+                TimePickerDialog tpd = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hour, int minute) {
+                        ed.set(Calendar.HOUR_OF_DAY, hour);
+                        ed.set(Calendar.MINUTE, minute);
+                        mEndTime.setText(hour + ":" + minute);
+
+                    }
+                }, cal.get(Calendar.HOUR),cal.get(Calendar.MINUTE),true);
+                tpd.show();
+            }
+        });
 
         builder.setTitle(R.string.dialog_newEvent)
                 .setView(view)
@@ -55,9 +155,9 @@ public class NewEventDialogFragment extends DialogFragment {
                 .setPositiveButton(R.string.save_event, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
-                        String title = mTitle.getText().toString();
 
-                        mListener.onDialogPositiveClick(title);
+                        String title = mTitle.getText().toString();
+                        mListener.onDialogPositiveClick(title, sd, ed);
 
                     }
                 })
