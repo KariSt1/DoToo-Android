78035420efddfcd4c225747ea7b0504cf2af99b1
gudlaugagnes <gak16@hi.is>
2021-03-15 23:25:17 +0000

    floating button

--- app/src/main/java/is/hi/hbv601g/dotoo/Networking/NetworkManager.java
+++ app/src/main/java/is/hi/hbv601g/dotoo/Networking/NetworkManager.java
@@ -40,7 +40,7 @@ public class NetworkManager {
 
     private static final String BASE_URL = "https://dotoo2.herokuapp.com/";
 
-   //  private static final String BASE_URL = "http://10.0.2.2:8080/";
+    //private static final String BASE_URL = "http://10.0.2.2:8080/";
 
     private static NetworkManager mInstance;
     private static RequestQueue mQueue;
