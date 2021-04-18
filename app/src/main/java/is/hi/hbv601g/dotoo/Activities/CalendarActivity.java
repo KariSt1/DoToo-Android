f44edac092b3ea56c2febcd136ec1db8ccacf000
gudlaugagnes <gak16@hi.is>
2021-04-18 22:48:13 +0000

    notification reminder for calendar

--- app/src/main/java/is/hi/hbv601g/dotoo/Activities/CalendarActivity.java
+++ app/src/main/java/is/hi/hbv601g/dotoo/Activities/CalendarActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.DialogFragment;
 
+import android.app.AlarmManager;
 import android.app.AlertDialog;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.RectF;
@@ -16,6 +19,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.content.Context;
+
 
 import com.alamkanak.weekview.DateTimeInterpreter;
 import com.alamkanak.weekview.MonthLoader;
@@ -268,27 +273,36 @@ public class CalendarActivity extends AppCompatActivity implements WeekView.Even
         mWeekView.notifyDatasetChanged();
 
         if(giveNotification) {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "CHANNEL_ID")
-                    .setSmallIcon(R.drawable.ic_calendar)
-                    .setContentTitle("Upcoming event!")
-                    .setContentText("You have an event coming up in an hour.")
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+            System.out.println("Erum að fara að setja hér á notification.");
+            addNotification(this,"My Notification");
+
         }
     }
 
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "name";
-            String description = "description";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("CHANNEL_ID", name, importance);
-            //channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
+    private void addNotification(Context context, String message) {
+
+        //int icon = R.drawable.ic_launcher;
+        long when = System.currentTimeMillis();
+        String appname = context.getResources().getString(R.string.app_name);
+        NotificationManager notificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Notification notification;
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+                new Intent(context, CalendarActivity.class), 0);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                context);
+        notification = builder.setContentIntent(contentIntent)
+                //.setSmallIcon(icon)
+                .setTicker(appname).setWhen(0)
+                .setAutoCancel(true).setContentTitle(appname)
+                .setContentText(message).build();
+
+        notificationManager.notify(0 , notification);
+
     }
+
+
 }
